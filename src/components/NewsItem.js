a9f13b4c83d85dead8c22387c2ecb27d191e93e0
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-04-12 14:03:38 +0530

    converted to function based component part 1

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,55 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
 
-    let a = date;
+  let a = date;
 
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absloute",
-              right: "0",
-            }}
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absloute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://images.hindustantimes.com/tech/img/2022/03/30/1600x900/34b37fd044c086eaf5cad7b613b70ce5_1645332046312_1648620612564.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "unknown" : author} on {a}
+            </small>
+          </p>
+          <a
+            rel="noreferrence"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
           >
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-          <img
-            src={
-              !imageUrl
-                ? "https://images.hindustantimes.com/tech/img/2022/03/30/1600x900/34b37fd044c086eaf5cad7b613b70ce5_1645332046312_1648620612564.jpg"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "unknown" : author} on {a}
-              </small>
-            </p>
-            <a
-              rel="noreferrence"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
